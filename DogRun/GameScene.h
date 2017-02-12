9851a5d4803deb528a9cf883bfe488df15ca4bd0
Anton Orzes <antonorzes@Antons-Mac-mini.local>
2017-02-12 12:18:53 +0100

    Initial Commit

---
+++ DogRun/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  DogRun
+//
+//  Created by Anton Orzes on 12/02/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
